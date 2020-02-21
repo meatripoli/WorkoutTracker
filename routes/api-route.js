fa5e035c93be990f26d0ef1bcaf3ce09ff23c052
Maria Elena Tripoli <meatripoli@gmail.com>
2020-02-21 13:00:52 -0600

    added the shells for the routes and model

--- routes/api-route.js
+++ routes/api-route.js
@@ -1,1 +1,22 @@
+const app = require("express").Router();
+const Workout = require("../models/workout-model.js");
 
+
+app.get("/api/workouts", (req, res) => {
+
+});
+
+app.get("/api/workouts/range", (req, res) => {
+
+});
+
+app.post("/api/workouts", ({ body }, res) => {
+
+});
+
+app.post("/api/workouts/:id", ({ body,params }, res) => {
+  let id = params.id;
+
+});
+
+module.exports = app;
