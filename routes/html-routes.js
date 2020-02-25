7e8e4f71420fd99aa5eb0f514b64486cc3f14dc8
Maria Elena Tripoli <meatripoli@gmail.com>
2020-02-25 11:32:21 -0600

    added routes to stats and excercise pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,6 +2,8 @@ const path = require("path");
 // var db = require("../models");
 // js template literal pages
 const indexPage = require("../views/index.js");
+const exercisePage = require("../views/exercise.js");
+const statsPage = require("../views/stats.js");
 
 
 module.exports = function(app) {
@@ -12,10 +14,16 @@ module.exports = function(app) {
   });
   app.get("/exercise", function(req, res) {
     console.log("inside /exercise route send to excercise page")
-    // res.send(indexPage.render());
+    res.send(exercisePage.render());
   });
   app.get("/stats", function(req, res) {
     console.log("inside /stats route send to dashboard page")
-    // res.send(indexPage.render());
+    res.send(statsPage.render());
+  });
+
+  // This needs to be the last route defined (render 404 for unmatched routes)
+  app.get("*", function(req, res) {
+    console.log("inside * route redirect to home page")
+    res.redirect("/")
   });
 };
